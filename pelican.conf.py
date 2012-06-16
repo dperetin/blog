38a1a6e38e7d0ca41a38117951a7b13c38a48e58
Dejan Peretin <dperetin@gmail.com>
2012-06-16 21:52:09 +0200

    Maknuti genericki linkovi

--- pelican.conf.py
+++ pelican.conf.py
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
 AUTHOR = u'Dejan Peretin'
-SITENAME = u"_dp.com"
+SITENAME = u"_dp"
 
 TIMEZONE = "Europe/Paris"
 
 GITHUB_URL = 'http://github.com/dperetin/'
-DISQUS_SITENAME = "_dp.com"
+DISQUS_SITENAME = "dperetin"
 PDF_GENERATOR = False
 REVERSE_CATEGORY_ORDER = True
 LOCALE = ""
@@ -14,12 +14,12 @@ DEFAULT_PAGINATION = 4
 FEED_RSS = 'feeds/all.rss.xml'
 CATEGORY_FEED_RSS = 'feeds/%s.rss.xml'
 
-LINKS = (('Biologeek', 'http://biologeek.org'),
-         ('Filyb', "http://filyb.info/"),
-         ('Libert-fr', "http://www.libert-fr.com"),
-         ('N1k0', "http://prendreuncafe.com/blog/"),
-         (u'Tarek Ziadé', "http://ziade.org/blog"),
-         ('Zubin Mithra', "http://zubin71.wordpress.com/"),)
+# LINKS = (('Biologeek', 'http://biologeek.org'),
+#          ('Filyb', "http://filyb.info/"),
+#          ('Libert-fr', "http://www.libert-fr.com"),
+#          ('N1k0', "http://prendreuncafe.com/blog/"),
+#          (u'Tarek Ziadé', "http://ziade.org/blog"),
+#          ('Zubin Mithra', "http://zubin71.wordpress.com/"),)
 
 SOCIAL = (('twitter', 'http://twitter.com/dperetin'),
           ('github', 'http://github.com/dperetin'),)
